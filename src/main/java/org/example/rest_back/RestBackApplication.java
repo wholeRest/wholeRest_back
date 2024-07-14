7477fca1d1e3dfabbb4b833642c7796e62adbaab
Kim <kimjy2000117@gmail.com>
2024-07-14 19:13:55 +0900

    first commit

---
+++ src/main/java/org/example/rest_back/RestBackApplication.java
@@ -0,0 +1,13 @@
+package org.example.rest_back;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RestBackApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RestBackApplication.class, args);
+	}
+
+}
---
+++ src/test/java/org/example/rest_back/RestBackApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.rest_back;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RestBackApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
