062dc9463f9964deb19eadc6a4c610501663b7a6
Kim <kimjy2000117@gmail.com>
2024-07-16 22:10:02 +0900

    edit restBackApplication

--- src/main/java/org/example/rest_back/RestBackApplication.java
+++ src/main/java/org/example/rest_back/RestBackApplication.java
@@ -5,13 +5,13 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class RestBackApplication {
-	static {
-		Dotenv dotenv = Dotenv.load();
-
-		System.setProperty("spring.datasource.url", "jdbc:mysql://" + dotenv.get("RDS_HOSTNAME") + ":" + dotenv.get("RDS_PORT") + "/" + dotenv.get("RDS_DB_NAME"));
-		System.setProperty("spring.datasource.username", dotenv.get("RDS_USERNAME"));
-		System.setProperty("spring.datasource.password", dotenv.get("RDS_PASSWORD"));
-	}
+//	static {
+//		Dotenv dotenv = Dotenv.load();
+//
+//		System.setProperty("spring.datasource.url", "jdbc:mysql://" + dotenv.get("RDS_HOSTNAME") + ":" + dotenv.get("RDS_PORT") + "/" + dotenv.get("RDS_DB_NAME"));
+//		System.setProperty("spring.datasource.username", dotenv.get("RDS_USERNAME"));
+//		System.setProperty("spring.datasource.password", dotenv.get("RDS_PASSWORD"));
+//	}
 	public static void main(String[] args) {
 		SpringApplication.run(RestBackApplication.class, args);
 	}
