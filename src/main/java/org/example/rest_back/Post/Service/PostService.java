4ea3958fd587ae3f11432f3b36a0e46d4831496e
E2YunJeong <dbsdbs3440@gmail.com>
2024-07-24 03:19:09 +0900

    🔨 Add PostService code
    Add PostService code

--- src/main/java/org/example/rest_back/Post/Service/PostService.java
+++ src/main/java/org/example/rest_back/Post/Service/PostService.java
@@ -5,6 +5,10 @@ import org.example.rest_back.Post.Dto.PostDto;
 import org.example.rest_back.Post.Repository.PostRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+import java.util.Optional;
 
 @Service
 public class PostService {
@@ -16,12 +20,59 @@ public class PostService {
         this.postRepository = postRepository;
     }
 
-    // Create
-    // Read All
-    // Read Specific Category
-    // Read Only One
-    // Update
-    // Delete
+    // Create, 게시물 생성
+    // @Transactional : 하나의 작업을 하나의 묶음으로 정의, 어떤 작업에서 오류가 발생했을 시
+    //                  오류가 발생한 묶음은 버리고 그 이전의 묶음으로 되돌리는 어노테이션
+    @Transactional
+    public Post registerPost(Post post) {
+        return postRepository.save(post);
+    }
+
+    // Read All, 모든 게시물 조회
+    public List<Post> getAllPosts() {
+        return postRepository.findAll();
+    }
+
+    // Read Specific Category, 특정 카테고리의 게시물들만 조회, 카테고리 값으로 조회
+    public List<Post> getPostsByCategory(String category) {
+        return postRepository.findByCategory(category);
+    }
+
+    // Read Only One, 단 하나의 게시물만 조회, id값으로 조회
+    // Optional : 값이 있을 수도 있고 없을 수도 있는 객체. null 참조를 피함
+    public Optional<Post> getPostById(Long id) {
+        return postRepository.findById(id);
+    }
+
+    // Update, 게시물 수정
+    @Transactional
+    public Post updatePost(Long id, PostDto postDto){
+        // Optional : 값이 있을 수도 있고 없을 수도 있는 객체. null 참조를 피함
+        // DB에 저장되어있는 게시물(수정하고자 하는 게시물)에 관한 정보를 postOptional 객체에 저장
+        Optional<Post> postOptional = postRepository.findById(id);
+
+        // postOptional 객체에 값이 존재하는지 확인
+        if (postOptional.isPresent()) {
+            // postOptional의 값을 반환하여 post 객체에 저장
+            Post post = postOptional.get();
+
+            // 객체에 수정사항 반영
+            post.update_post(postDto);
+
+            // DB에 수정한 내용 저장
+            return postRepository.save(post);
+        }
+        // postOptional 객체에 값이 존재하지 않으면 예외처리
+        else {
+            throw new RuntimeException(("Post not found with id " + id));
+        }
+    }
+
+    // Delete, 게시물 삭제
+    @Transactional
+    public void deletePost(Long id){
+        postRepository.deleteById(id);
+    }
 
     // Entity -> Dto
     private PostDto convertToDto(Post post){
