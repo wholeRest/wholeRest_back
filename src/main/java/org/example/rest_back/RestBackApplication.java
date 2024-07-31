83172dbf0b4db827ad2260f08b06c0f686f6c5d9
Kim <kimjy2000117@gmail.com>
2024-08-01 01:54:00 +0900

    🔧 Update configuration files
    배포 환경에 맞게 환경변수 및 gradle 수정

--- src/main/java/org/example/rest_back/RestBackApplication.java
+++ src/main/java/org/example/rest_back/RestBackApplication.java
@@ -5,21 +5,21 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class RestBackApplication {
-	static {
-		Dotenv dotenv = Dotenv.load();
-
+//	static {
+//		Dotenv dotenv = Dotenv.load();
+//
 //		System.setProperty("spring.datasource.url", "jdbc:mysql://" + dotenv.get("RDS_HOSTNAME") + ":" + dotenv.get("RDS_PORT") + "/" + dotenv.get("RDS_DB_NAME"));
 //		System.setProperty("spring.datasource.username", dotenv.get("RDS_USERNAME"));
 //		System.setProperty("spring.datasource.password", dotenv.get("RDS_PASSWORD"));
-
-		System.setProperty("AWS_S3_BUCKET", dotenv.get("AWS_S3_BUCKET"));
-
-		System.setProperty("AWS_ACCESS_KEY", dotenv.get("AWS_ACCESS_KEY"));
-		System.setProperty("AWS_SECRET_KEY", dotenv.get("AWS_SECRET_KEY"));
-
-		System.setProperty("JWT_SECRET_KEY", dotenv.get(("JWT_SECRET_KEY")));
-		System.setProperty("MAIL_PASSWORD", dotenv.get(("MAIL_PASSWORD")));
-	}
+//
+//		System.setProperty("AWS_S3_BUCKET", dotenv.get("AWS_S3_BUCKET"));
+//
+//		System.setProperty("AWS_ACCESS_KEY", dotenv.get("AWS_ACCESS_KEY"));
+//		System.setProperty("AWS_SECRET_KEY", dotenv.get("AWS_SECRET_KEY"));
+//
+//		System.setProperty("JWT_SECRET_KEY", dotenv.get(("JWT_SECRET_KEY")));
+//		System.setProperty("MAIL_PASSWORD", dotenv.get(("MAIL_PASSWORD")));
+//	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(RestBackApplication.class, args);
