ee7f2c0b0826d47911cceebfc73936c3a8ce250a
Kim <kimjy2000117@gmail.com>
2024-07-31 18:01:20 +0900

    🐛 Fix EventRepository
    EventRespositroy 오류 수정

--- src/main/java/org/example/rest_back/mypage/repository/EventRepository.java
+++ src/main/java/org/example/rest_back/mypage/repository/EventRepository.java
@@ -1,7 +1,6 @@
 package org.example.rest_back.mypage.repository;
 
 import org.example.rest_back.mypage.entity.Event;
-import org.example.rest_back.mypage.entity.Member;
 import org.example.rest_back.user.domain.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
@@ -13,6 +12,6 @@ import java.util.List;
 public interface EventRepository extends JpaRepository<Event, Integer> {
     List<Event> findByUser(User user);
 
-    @Query("SELECT e FROM Event e WHERE e.user = :member AND e.date = :date")
+    @Query("SELECT e FROM Event e WHERE e.user = :user AND e.date = :date")
     List<Event> findAllByUserAndDate(@Param("user") User user, @Param("date") LocalDate date);
 }
